33aedc567fe61cc0f47a12ce72b20fb6c3e379a7
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-11 20:41:04 +0300

    make RulesVC. Add labels and constraints.

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -63,11 +63,15 @@ class CardSelectionVC: UIViewController {
     func configureRulesButton() {
         view.addSubview(rulesButton)
         
+        rulesButton.addTarget(self, action: #selector(presentRulesVC), for: .touchUpInside)
         NSLayoutConstraint.activate([
             rulesButton.widthAnchor.constraint(equalToConstant: 115),
             rulesButton.heightAnchor.constraint(equalToConstant: 50),
             rulesButton.trailingAnchor.constraint(equalTo: stopButton.trailingAnchor),
             rulesButton.topAnchor.constraint(equalTo: stopButton.bottomAnchor, constant: 20)
                                     ])
+    }
+    @objc func presentRulesVC() {
+        present(RulesVC(), animated: true)
     }
 }
--- CardWorkout-Programmatic/RulesVC.swift
+++ CardWorkout-Programmatic/RulesVC.swift
@@ -8,22 +8,66 @@
 import UIKit
 
 class RulesVC: UIViewController {
-
+    
+    //MARK: Create labels
+    let titleLabel = UILabel()
+    let rulesLabel = UILabel()
+    let exerciseLabel = UILabel()
+    let rulesText = "The value of each card represents the number of exercise you do.\n\nJ = 11, Q =12, K = 13, A =14"
+    let exerciseText = "♤ = Push-up\n\n♡ = Sit-up\n\n♧ = Burpees\n\n♢ = Jumping Jacks"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureTitleLabel()
+        configureRulesLabel()
+        configureExerciseLabel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Create functions
+    func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Rules"
+        titleLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        titleLabel.textAlignment = .center
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+        ])
     }
-    */
-
+    func configureRulesLabel() {
+        view.addSubview(rulesLabel)
+        rulesLabel.translatesAutoresizingMaskIntoConstraints = false
+        rulesLabel.text = rulesText
+        rulesLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        rulesLabel.textAlignment = .center
+        rulesLabel.lineBreakMode = .byWordWrapping
+        rulesLabel.numberOfLines = 0
+        
+        
+        NSLayoutConstraint.activate([
+            rulesLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25),
+            rulesLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: 30),
+            rulesLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor, constant: -30)
+            
+        ])
+    }
+    func configureExerciseLabel() {
+        view.addSubview(exerciseLabel)
+        exerciseLabel.translatesAutoresizingMaskIntoConstraints = false
+        exerciseLabel.text = exerciseText
+        exerciseLabel.font = .systemFont(ofSize: 19, weight: .semibold)
+        exerciseLabel.numberOfLines = 0
+        
+        
+        NSLayoutConstraint.activate([
+            exerciseLabel.topAnchor.constraint(equalTo: rulesLabel.bottomAnchor, constant: 60),
+            exerciseLabel.widthAnchor.constraint(equalToConstant: 200),
+            exerciseLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
 }
